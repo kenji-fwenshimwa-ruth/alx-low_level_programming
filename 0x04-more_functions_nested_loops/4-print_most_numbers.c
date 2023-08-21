3dcb164a6dc3d73559eb299bb358efac4b1b2a3f
kenji-fwenshimwa-ruth <kenjifwenshimwa2@gmail.com>
2023-08-20 17:17:42 -0700

    I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		if (number == '2' || number == '4')
+			;
+		else
+			_putchar(number);
+	}
+	_putchar('\n');
+}
